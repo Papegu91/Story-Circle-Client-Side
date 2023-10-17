367628f50307b98c7a0bd81f5eedb340e9797b95
peter gudoi <petergudoi@gmail.com>
2023-10-17 11:05:08 +0300

    commit

--- public/models.py
+++ public/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validatesgit 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
